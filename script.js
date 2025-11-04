306eb59e901c9d222957f723cda088146c912e30
okwjdkyqh <pk6224prvv@privaterelay.appleid.com>
2025-11-04 20:03:18 +0300

    passandstuff

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("translateee").addEventListener("click", function() {
+
+  document.getElementById("surname").innerText = "Raimova";
+  document.getElementById("name").innerText = "Polina";
+  document.getElementById("dad_name").innerText = "Vladimirovna";
+  document.getElementById("sex").innerText = "female";
+  document.getElementById("birth_place").innerText = "Moscow city";
+  document.getElementById("MVD").innerText = "GU MVD of Moscow";
+});
